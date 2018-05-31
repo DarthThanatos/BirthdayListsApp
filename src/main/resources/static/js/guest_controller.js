972c978de290f59b338cd2de4496341ce3f786cb
DarthThanatos <robert.bielas@interia.pl>
2018-05-31 17:11:01 +0200

    angular - header of list square done

--- src/main/resources/static/js/guest_controller.js
+++ src/main/resources/static/js/guest_controller.js
@@ -5,4 +5,15 @@ GuestHome.controller('GuestController', function GuestController($scope) {
         console.log("fb clicked")
     }
 
+    $scope.all = function(){
+        console.log("all clicked")
+    }
+
+    $scope.notReserved = function(){
+        console.log("not reserved clicked")
+    }
+
+    $scope.reserved = function(){
+        console.log("reserved clicked")
+    }
 })
