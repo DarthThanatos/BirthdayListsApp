5854cc90bef13f4bf917989b261830d13ba15fa9
DarthThanatos <robert.bielas@interia.pl>
2018-05-31 12:06:08 +0200

    restructured resources subfolder tree & angular init

--- src/main/js/BirthdayGuyHome.js
+++ src/main/js/BirthdayGuyHome.js
@@ -7,7 +7,7 @@ export default class BirthdayGuyHome extends React.Component{
              <div>
 
                 <div style={{ fontSize:"20px", width:"100%", height:"30px", display: "flex",  flexDirection:"row", alignItems:"center", justifyContent: "center"}}>
-                    Solenizat - strona główna
+                    Solenizant - strona główna
                 </div>
 		        <img src="img/background_transparent.png" alt="birthday party image"/>
              </div>
